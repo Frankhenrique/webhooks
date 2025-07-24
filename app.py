f4341f1132589e75fdebbf06ecce2e9fbf949cc7
Frankhenrique <54981164+Frankhenrique@users.noreply.github.com>
2025-07-24 17:30:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def webhook_rcs():
     dados = request.get_json()
     print("🔄 Redirecionando para o servidor interno...")
     try:
-        r = requests.post("http://177.26.126.215:5000/webhook_rcs", json=dados, timeout=3)
+        r = requests.post("http://177.26.126.215:5000/webhook_rcs", json=dados)
         print("✅ Encaminhado:", r.status_code)
     except Exception as e:
         print("❌ Erro ao enviar:", str(e))
